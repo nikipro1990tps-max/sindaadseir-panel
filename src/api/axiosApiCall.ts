6d63dff523ace6865ab012c2096a81710b52eb4f
ehsan.harani <ehsan.harani@gmail.com>
2025-10-30 01:06:34 +0330

    add axiosApiCall and imporve api services

---
+++ src/api/axiosApiCall.ts
@@ -0,0 +1,88 @@
+import { MyToast } from "../components/Elements/MyToast";
+import axiosInstance from "./axiosConfig";
+
+let abortController: any;
+
+
+const axiosApiCall = {
+
+    async get(url: string, filters = {}) {
+
+
+        if (!abortController) {
+            abortController = new AbortController()
+        }
+
+        try {
+            const response = await axiosInstance.get(url, {
+                params: filters,
+                signal: abortController.signal,
+
+            })
+            return response.data.result
+        } catch (error) {
+            MyToast.error(error)
+            throw error
+        }
+
+    },
+
+
+    async post(url: string, body = {}) {
+        try {
+            const response = await axiosInstance.post(url, body)
+            return response.data.result
+        } catch (error) {
+            MyToast.error(error)
+            throw error
+        }
+    },
+
+    async postFormData(url: string, formData: FormData) {
+        try {
+            const response = await axiosInstance.post(url, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+       
+            return response.data.result
+        } catch (error) {
+            MyToast.error(error)
+            throw error
+        }
+    },
+
+    async patch(url: string, body = {}) {
+        try {
+            const response = await axiosInstance.patch(url, body)
+
+            return response.data.result
+        } catch (error) {
+            MyToast.error(error)
+            throw error
+        }
+    },
+
+    async put(url: string, body = {}) {
+        try {
+            const response = await axiosInstance.put(url, body)
+            return response.data.result
+        } catch (error) {
+            MyToast.error(error)
+            throw error
+        }
+    },
+
+    async delete(url: string) {
+        try {
+            const response = await axiosInstance.delete(url)
+            return response.data.result
+        } catch (error) {
+            MyToast.error(error)
+            throw error
+        }
+    }
+}
+
+export default axiosApiCall
--- src/api/services/auth.api.tsx
+++ src/api/services/auth.api.tsx
@@ -1,28 +1,16 @@
-import { MyToast } from "../../components/Elements/MyToast";
-import axiosInstance from "../axiosConfig"
+import axiosApiCall from "../axiosApiCall";
 
 // سرویس‌های خاص
 export const authApiService = {
-    async login(body: { identifier: string, password: string }, signal: any = null) {
-
-        try {
-            const response = await axiosInstance.post('/user/auth/login/admin', body, signal);
-            return response.data.result
-        } catch (error) {
-            // throw error
-            MyToast.error(error)
-        }
+    async login(body: { identifier: string, password: string }) {
+        return await axiosApiCall.post('/user/auth/admin/login', body)
     },
 
 
     async checkLogin() {
-        try {
-            const response = await axiosInstance.get("/user/auth")
-            return response.data?.result
-        } catch (error) {
-            MyToast.error(error)
-            throw error
-        }
+
+        return await axiosApiCall.get("/user/auth/admin")
+
     },
 
 
